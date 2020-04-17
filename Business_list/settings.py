6df936462aba25274f192b635af3e34783652f95
Ala Mansouri <mansouriwhafidoumansouri@gmail.com>
2020-04-17 11:35:44 +0100

    2nd day of attempting to host this app on heroku LOL

--- Business_list/settings.py
+++ Business_list/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "default_value")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['business-list.herokuapp.com']
+ALLOWED_HOSTS = ['https://business-list.herokuapp.com/']
 
 
 # Application definition
